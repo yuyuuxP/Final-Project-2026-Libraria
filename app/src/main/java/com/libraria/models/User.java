a26c1e66ef92f6ffcb54743f85dcc8149c37a6f0
Patriciusss <patriciusdanduru@gmail.com>
2026-05-28 22:29:21 +0800

    refactor: menyusun ulang agar menjadi parent class

--- app/src/main/java/com/libraria/models/User.java
+++ app/src/main/java/com/libraria/models/User.java
@@ -1,47 +1,29 @@
 package com.libraria.models;
 
 public class User {
-    private String username;
-    private String password;
-    private String secretquestion;
-    private String secretanswer;
+    protected String email;
+    protected String password;
 
-    public User(String username, String password, String secretquestion, String secretanswer) {
-        this.username = username;
+    public User() {}
+
+    public User(String email, String password) {
+        this.email = email;
         this.password = password;
-        this.secretquestion = secretquestion;
-        this.secretanswer = secretanswer;
     }
 
-    public String getUsername() {
-        return username;
+    public String getEmail() {
+        return email;
     }
 
     public String getPassword() {
         return password;
     }
 
-    public String getSecretquestion() {
-        return secretquestion;
-    }
-    
-    public String getSecretanswer() {
-        return secretanswer;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
+    public void setEmail(String email) {
+        this.email = email;
     }
 
     public void setPassword(String password) {
         this.password = password;
-    }
-
-    public void setSecretquestion(String secretquestion) {
-        this.secretquestion = secretquestion;
-    }
-
-    public void setSecretanswer(String secretanswer) {
-        this.secretanswer = secretanswer;
     }
 }
