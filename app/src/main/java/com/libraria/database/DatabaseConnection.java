542f891e8279225544eff059bd743743eed67707
Patriciusss <patriciusdanduru@gmail.com>
2026-05-28 22:37:50 +0800

    feat: menambah kolom role pada tabel user dan menambah akun admin kedalam database

--- app/src/main/java/com/libraria/database/DatabaseConnection.java
+++ app/src/main/java/com/libraria/database/DatabaseConnection.java
@@ -27,9 +27,12 @@ public class DatabaseConnection {
                 email TEXT NOT NULL UNIQUE, 
                 password TEXT NOT NULL, 
                 secretquestion TEXT NOT NULL, 
-                secretanswer TEXT NOT NULL
+                secretanswer TEXT NOT NULL,
+                role TEXT NOT NULL DEFAULT 'member'
                 )
             """);
+
+            stmt.execute("INSERT OR IGNORE INTO users (email, password, secretquestion, secretanswer, role) VALUES ('admin@gmail.com', 'passwordkamu', '-', '-', 'admin')");
 
             stmt.execute("""
                 CREATE TABLE IF NOT EXISTS books(
