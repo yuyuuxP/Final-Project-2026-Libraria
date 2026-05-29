07d234ab9b9c38b65b8401fda3becc843de71f1e
yuyuuxP <nayu82004@gmail.com>
2026-05-30 00:37:17 +0800

    feat: membuat logika fitur pengembalian buku -Keisya

--- app/src/main/java/com/libraria/services/PengembalianService.java
+++ app/src/main/java/com/libraria/services/PengembalianService.java
@@ -1,5 +1,11 @@
 package com.libraria.services;
 
+import com.libraria.dao.PengembalianDAO;
+
 public class PengembalianService {
-    
+    private PengembalianDAO pengembalianDAO = new PengembalianDAO();
+
+    public boolean eksekusiPengembalian(String judul) {
+        return pengembalianDAO.kembalikanBuku(judul);
+    }
 }
