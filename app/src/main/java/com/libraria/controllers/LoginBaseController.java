f6bf5ab2e8b550d97362c969133745d705284974
Patriciusss <patriciusdanduru@gmail.com>
2026-05-29 23:48:11 +0800

    refactor: menguba sedikit penulisan code

--- app/src/main/java/com/libraria/controllers/LoginBaseController.java
+++ app/src/main/java/com/libraria/controllers/LoginBaseController.java
@@ -2,21 +2,25 @@ package com.libraria.controllers;
 
 import com.libraria.dao.UserDAO;
 import com.libraria.utils.Validator;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.scene.Parent;
 
 public abstract class LoginBaseController {
     UserDAO data = new UserDAO();
     Validator validator = new Validator();
 
-    protected void loadScene(Stage stage, javafx.scene.Parent root, String title) {
+    protected void loadScene(Stage stage, Parent root, String title) {
         boolean wasMaximized = stage.isMaximized();
-        javafx.scene.Scene scene = new javafx.scene.Scene(root, 600, 600);
+        Scene scene = new Scene(root, 600, 600);
         stage.setScene(scene);
         stage.setTitle(title);
+
         if (!wasMaximized) {
             stage.setWidth(600);
             stage.setHeight(600);
         }
+        
         stage.setMaximized(wasMaximized);
         stage.show();
     }
