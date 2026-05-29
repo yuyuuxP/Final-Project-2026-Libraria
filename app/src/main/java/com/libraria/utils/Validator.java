62187a7456e107d2c15c95204ad4560d007353c4
Patriciusss <patriciusdanduru@gmail.com>
2026-05-29 16:51:20 +0800

    fix: memperbaiki logika validator email

--- app/src/main/java/com/libraria/utils/Validator.java
+++ app/src/main/java/com/libraria/utils/Validator.java
@@ -1,6 +1,30 @@
 package com.libraria.utils;
 
 public class Validator {
+    public boolean isEmailValid(String email) {
+        if (email.contains("@")) {
+            String[] parts = email.split("@");
+
+            if (parts.length == 2 && !parts[0].isEmpty()) {
+                if (email.endsWith("@gmail.com") || email.endsWith("@libraria.com")) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        }
+        
+        return false;
+    }
+
+    public boolean isSecretQuestionValid(String SecretQuestion) {
+        if (SecretQuestion.length() > 1 && SecretQuestion.endsWith("?")) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
     public boolean isFieldNotEmpty(String text) {
         if (text.isEmpty()) {
             return false;
